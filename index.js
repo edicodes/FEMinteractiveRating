c83203dd15c8284a139022dd14897e660604edbb
edicodes <125201811+edicodes@users.noreply.github.com>
2023-11-11 22:25:44 -0700

    final edit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
+// select the span holding the rating numbers
 const ratings = document.querySelectorAll('[data-rating');
-// let text = document.querySelector(".select").innerHtml;
 
-
+//for each span rating, listen for a click, remove all classes of .select that change the bg color to indicate selection, then add it back to the one clicked. 
+//also output the rating clicked inner text content into the span on the thank you page with the id #rating-selected
 ratings.forEach(rating => {
     rating.addEventListener('click', () => {
         ratings.forEach(rating => {
@@ -9,13 +11,12 @@ ratings.forEach(rating => {
         })
         rating.classList.add('select')   
 
-        // document.getElementById('rating-selected').textContent = text 
+        document.getElementById('rating-selected').textContent = rating.textContent
     })
 })
 
 
-
-
+//allow the button to move to the next page and add/remove the class .hidden from the respective divs
 function changeState() {
     let initialState = document.querySelector('.rating-state')
     let thankYouState = document.querySelector('.thank-you-state')
