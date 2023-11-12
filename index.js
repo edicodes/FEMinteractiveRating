86152e5f53ae748afb91f34a8fb12cc4bc3f764c
edicodes <125201811+edicodes@users.noreply.github.com>
2023-11-11 20:33:57 -0700

    e

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 document.querySelectorAll('.number').forEach(rating => {
     rating.addEventListener('click', () => {
-        rating.classList.toggle('select');
+        rating.classList.remove('select');
+        rating.classList.add('select')
     })
 })
 
-console.log('this works')
+
+function changeState() {
+    let initialState = document.querySelector('.rating-state')
+    let thankYouState = document.querySelector('.thank-you-state')
+
+    initialState.classList.add('hidden')
+    thankYouState.classList.remove('.hidden')
+}
